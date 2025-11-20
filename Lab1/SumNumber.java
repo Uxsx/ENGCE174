16058e144ae4396be2985686409fb6a1242ccad4
Uxsx <snowlevelup@gmail.com>
2025-11-20 11:45:21 +0700

    edit Lab1.1

--- Lab1/SumNumber.java
+++ Lab1/SumNumber.java
@@ -2,7 +2,7 @@ import java.util.Scanner ;
 
 public class SumNumber {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in) ;
 
         int numOne = input.nextInt() ;
         int numTwo = input.nextInt() ;
