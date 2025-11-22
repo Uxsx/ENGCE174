e47acc487233d3ed0402ff29ee946910fb11d360
Uxsx <snowlevelup@gmail.com>
2025-11-22 22:36:25 +0700

    add 1.7

---
+++ Lab1/SumLoopN.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class SumLoopN {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("กรุณาตัวเลขที่ต้องการป้อน: ");
+        int n = scanner.nextInt();
+
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            int number = scanner.nextInt();
+            sum += number;
+        }
+
+        System.out.println(sum);
+
+        scanner.close();
+    }
+}
