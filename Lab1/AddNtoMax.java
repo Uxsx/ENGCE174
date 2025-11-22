2db32cf653d391a5a5eae687822fb109f9ea3b3d
Uxsx <snowlevelup@gmail.com>
2025-11-22 22:40:55 +0700

    add 1.8

---
+++ Lab1/AddNtoMax.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class AddNtoMax {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("กรุณาระบุจำนวนตัวเลข : ");
+        int n = scanner.nextInt();
+
+        int[] numbers = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            numbers[i] = scanner.nextInt();
+        }
+
+        int max = numbers[0];
+
+        for (int i = 1; i < n; i++) {
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+        }
+
+        System.out.println("ค่าที่มากที่สุด คือ: " + max);
+
+        scanner.close();
+    }
+}
--- Lab1/SumLoopN.java
+++ Lab1/SumLoopN.java
@@ -1,21 +1,21 @@
-import java.util.Scanner;
+import java.util.Scanner ;
 
 public class SumLoopN {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        System.out.print("กรุณาตัวเลขที่ต้องการป้อน: ");
-        int n = scanner.nextInt();
+        Scanner scanner = new Scanner(System.in) ;
 
-        int sum = 0;
+        System.out.print("กรุณาตัวเลขที่ต้องการป้อน: ") ;
+        int n = scanner.nextInt() ;
+
+        int sum = 0 ;
 
         for (int i = 1; i <= n; i++) {
-            int number = scanner.nextInt();
-            sum += number;
+            int number = scanner.nextInt() ;
+            sum += number ;
         }
 
-        System.out.println(sum);
+        System.out.println(sum) ;
 
-        scanner.close();
+        scanner.close() ;
     }
 }
