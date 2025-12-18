ccd7270e71974344a76cff171b3c51de7d657667
Uxsx <snowlevelup@gmail.com>
2025-12-18 10:50:22 +0700

    lab4

---
+++ Lab4/lab41.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+class Book {
+    private String title;
+    private String author;
+
+
+
+    public Book(String title) {
+        this.title = title;
+        this.author = "Unknown";
+    }
+
+    public Book(String title,String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    public void displayInfo() {
+        System.out.println("Title: " + this.title + "," + "Author: " + this.author);
+    }
+}
+
+public class lab41 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Select Mode 1 or 2: ");
+        int mode = scanner.nextInt();
+
+        if(mode == 1) {
+            scanner.nextLine();
+            String getN = scanner.nextLine();
+            Book book = new Book(getN);
+            book.displayInfo();
+        } else if(mode == 2) {
+            scanner.nextLine();
+            String getN = scanner.nextLine();
+            String getA = scanner.nextLine();
+            Book book = new Book(getN, getA); 
+            book.displayInfo();
+        } else {
+            System.out.println("Error pls Select Mode 1 or 2");
+        }
+
+        scanner.close();
+    }
+}
